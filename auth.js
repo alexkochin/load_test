d5c05a48323a475817ed559e976614fc90f4faed
301thspartan <nahuagawa@gmail.com>
2020-02-10 23:05:19 +0200

    auth.js as get token function

--- auth.js
+++ auth.js
@@ -9,16 +9,12 @@ export const options = {
 
 export default function() {
   let response;
-  let url = "https://testing.carggo.com/auth/realms/CarggoSecurityRealm/protocol/openid-connect/token"
-  let payload = "client_id=carggo-mdm-app&grant_type=password&client_secret=78aabf56-7b86-4c92-9c1d-355f154c0619&username=chocolate-factory%40carggo.com&password=C%40rggo2%40!9"
-  let params = { headers: { "Content-Type": "application/x-www-form-urlencoded; charset=utf-8", "Host": "testing.carggo.com", "Referer": "https://testing.carggo.com/" } }
-  // Get token
-  response = http.post(url, payload, params  )
+  let keycloak_url = "https://testing.carggo.com/auth/realms/CarggoSecurityRealm/protocol/openid-connect/token"
+  let auth_payload = "client_id=carggo-mdm-app&grant_type=password&client_secret=78aabf56-7b86-4c92-9c1d-355f154c0619&username=chocolate-factory%40carggo.com&password=C%40rggo2%40!9"
+  let auth_headers = { headers: { "Content-Type": "application/x-www-form-urlencoded; charset=utf-8", "Host": "testing.carggo.com", "Referer": "https://testing.carggo.com/" } }
 
-//  if (response.status === 200) {
-//      console.log("Response: " + response.body + ": " + response.body)
-//    }
-  let token = response.access_token
-    console.log("Token: " + token)
-  sleep(1);
+  response = http.post(keycloak_url, auth_payload, auth_headers  )
+  let access_token = response.json().access_token;
+  //  console.log("Access Token: " + access_token)
+  return access_token
 }
---
+++ get_rate.js
@@ -0,0 +1,15 @@
+import { sleep } from "k6";
+import http from "k6/http";
+import access_token from "./auth.js";
+
+export const options = {
+  stages: [
+    { duration: "10s", target: 1 }
+  ]
+};
+
+export default function() {
+  let   mytok = access_token()
+    console.log("Hoora " + mytok)
+
+}
