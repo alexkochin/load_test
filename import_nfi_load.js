cf6d4df4d3e026d485b0e54bfb1c72f026f03fb7
alexkochin <alex.kochin@carggo.com>
2020-02-11 16:31:51 +0200

    wip

--- import_nfi_load.js
+++ import_nfi_load.js
@@ -1,5 +1,9 @@
 import { sleep } from "k6";
 import http from "k6/http";
+import access_header from "./auth.js";
+
+const auth_payload = JSON.parse(open("./nfi_payload.json"))
+
 
 export const options = {
   stages: [
@@ -9,8 +13,7 @@ export const options = {
 
 export default function() {
   let response
-  let keycloak_url = "https://testing.carggo.com/auth/realms/CarggoSecurityRealm/protocol/openid-connect/token"
-  let auth_payload = "client_id=carggo-mdm-app&grant_type=password&client_secret=78aabf56-7b86-4c92-9c1d-355f154c0619&username=chocolate-factory%40carggo.com&password=C%40rggo2%40!9"
+  let nfi_load_import_url = "http://datalake.testing.swagger.carggo.int/datalake/loads/import"
   let auth_headers = { headers: { "Content-Type": "application/x-www-form-urlencoded; charset=utf-8", "Host": "testing.carggo.com", "Referer": "https://testing.carggo.com/" } }
 
   response = http.post(keycloak_url, auth_payload, auth_headers  )
