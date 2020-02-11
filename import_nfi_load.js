df580b3be73c94e55662437dadfd374b5ef45b39
alexkochin <alex.kochin@carggo.com>
2020-02-11 18:22:08 +0200

    wip

--- import_nfi_load.js
+++ import_nfi_load.js
@@ -1,6 +1,11 @@
 import { sleep } from "k6";
 import http from "k6/http";
 import access_header from "./auth.js";
+import { Trend } from "k6/metrics";
+
+
+var myTrend = new Trend("waiting_time");
+
 
 const payload = JSON.parse(open("./nfi_payload.json"))
 
@@ -8,18 +13,20 @@ const payload = JSON.parse(open("./nfi_payload.json"))
 
 export const options = {
   stages: [
-    { duration: "40s", target: 1 }
+    { duration: "5s", target: 1 }
   ]
 };
 
 export default function() {
   let response
-  let nfi_load_import_url = "http://datalake.testing.swagger.carggo.int/datalake/loads/import"
-  let auth_headers = { headers: { "Content-Type": "application/json", "Host": "testing.carggo.com", "Referer": "https://testing.carggo.com/" } }
+  let nfi_load_import_url = "http://datalake.testing.swagger.carggo.int/datalake/datalake/loads"
+  let headers = { headers: { "Content-Type": "application/json" } }
 
-  response = http.post(nfi_load_import_url, payload  )
+  response = http.post(nfi_load_import_url, JSON.stringify(payload), headers  )
 
-//  let access_header = "Bearer " + response.json().access_token;
-    console.log(response.body)
+    //console.log(response.status + ": " + response.body);
+    //console.log("Response time was " + String(response.timings.duration) + " ms");
+    //console.log("Total timings: " + myTrend.add(response.timings.waiting));
+    myTrend.add(response.timings.waiting)
 
 }
