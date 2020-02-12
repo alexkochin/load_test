1e62ceff3555dd4dd172003eec148dd9c7f41057
Aleksander V. Kochin <alex.kochin@carggo.local>
2020-02-12 15:41:40 +0200

    options added

--- import_nfi_load.js
+++ import_nfi_load.js
@@ -10,16 +10,20 @@ var myTrend = new Trend("waiting_time");
 const payload = JSON.parse(open("./test_data/nfi_payload.json"))
 
 //  console.log(JSON.stringify(payload))
-const auth_header = access_header()
+export function setup() {
+  const auth_header = access_header();
+  return auth_header
+};
 
 
 export const options = {
   stages: [
     { duration: "3s", target: 1 }
-  ]
+  ],
+
 };
 
-export default function() {
+export default function(auth_header) {
   let response
   let nfi_load_import_url = "http://datalake.testing.swagger.carggo.int/v1/datalake/loads"
   let headers = { headers: { "Content-Type": "application/json", "authorization": auth_header } }
