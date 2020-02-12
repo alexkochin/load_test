a3541610ed07fb4ec9fa7c85555b353ac78e10a1
alexkochin <alex.kochin@carggo.com>
2020-02-12 12:22:51 +0200

    added check(response) for 201 response

--- import_nfi_load.js
+++ import_nfi_load.js
@@ -1,4 +1,4 @@
-import { sleep } from "k6";
+import { check, sleep } from "k6";
 import http from "k6/http";
 import access_header from "./auth.js";
 import { Trend } from "k6/metrics";
@@ -14,7 +14,7 @@ const payload = JSON.parse(open("./test_data/nfi_payload.json"))
 
 export const options = {
   stages: [
-    { duration: "5s", target: 1 }
+    { duration: "180s", target: 50 }
   ]
 };
 
@@ -26,9 +26,13 @@ export default function() {
 
   response = http.post(nfi_load_import_url, JSON.stringify(payload), headers  )
 
-    console.log(response.status + ": " + JSON.stringify(response.body));
+    //console.log(response.status + ": " + JSON.stringify(response.body));
     //console.log("Response time was " + String(response.timings.duration) + " ms");
     //console.log("Total timings: " + myTrend.add(response.timings.waiting));
     myTrend.add(response.timings.waiting)
+
+    check(response, {
+      "is status 201": (r) => r.status === 201
+  });
 
 }
