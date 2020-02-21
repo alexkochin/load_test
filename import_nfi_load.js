a7e291ee4019cf9f5ec0a0f40dfadbe9caa38c72
alexkochin <alex.kochin@carggo.com>
2020-02-21 14:01:08 +0200

    added duration metric

--- import_nfi_load.js
+++ import_nfi_load.js
@@ -1,13 +1,17 @@
 import { check, sleep } from "k6";
 import http from "k6/http";
 import access_header from "./auth.js";
-import { Trend } from "k6/metrics";
+import { Trend , Rate} from "k6/metrics";
 
+export const options = {
+  stages: [
+    { duration: "30s", target: 1000 }
+  ]
+};
 
-var myTrend = new Trend("waiting_time");
-
-
-const payload = JSON.parse(open("./test_data/nfi_payload.json"))
+var duration_trend = new Trend("duration_time");
+var fail_rate = new Rate("failed requests");
+const payload = JSON.parse(open("./test_data/nfi_payload.json"));
 
 //  console.log(JSON.stringify(payload))
 export function setup() {
@@ -15,20 +19,21 @@ export function setup() {
   return auth_header
   };
 
-export const options = {
-  stages: [
-    { duration: "200s", target: 1000 }
-  ]
-};
+
 
 export default function(auth_header) {
   let response
   let nfi_load_import_url = "https://testing.carggo.com/v1/datalake/loads"
-  let headers = { headers: { "Content-Type": "application/json", "accept": "application/json", "authorization": auth_header } }
+  let headers = { headers: { "Content-Type": "application/json", "authorization": auth_header } }
 
-  response = http.post(nfi_load_import_url, JSON.stringify(payload), headers  )
-//  console.log(response.status)
-    myTrend.add(response.timings.waiting)
+    response = http.post(nfi_load_import_url, JSON.stringify(payload), headers  )
+
+    //console.log(response.status + ": " + JSON.stringify(response.body));
+    //console.log("Response time was " + String(response.timings.duration) + " ms");
+    //console.log("Total timings: " + myTrend.add(response.timings.waiting));
+
+    duration_trend.add(response.timings.duration)
+    fail_rate.add(response.status !== 201)
 
     check(response, {
       "is status 201": (r) => r.status === 201
