9d5c110660675cbe3a82ba6c7e9661dfbc5e6c5c
Alex Kochin <alex.kochin@carggo.com>
2020-02-27 19:49:36 +0200

    Fixed library path

--- import_nfi_load.js
+++ import_nfi_load.js
@@ -2,7 +2,7 @@ import { check, sleep } from "k6";
 import http from "k6/http";
 import access_header from "./auth.js";
 import { Trend , Rate} from "k6/metrics";
-import papaparse from '.node_modules/papaparse/papaparse.js';
+import papaparse from './node_modules/papaparse/papaparse.js';
 
 
 export const options = {
@@ -22,18 +22,13 @@ export function setup() {
   };
 
 
-
+//Main test body starts here
 export default function(auth_header) {
   let response
   let nfi_load_import_url = "https://testapi.carggo.com/v1/datalake/loads"
   let headers = { headers: { "Content-Type": "application/json", "authorization": auth_header } }
 
     response = http.post(nfi_load_import_url, JSON.stringify(payload), headers  )
-
-    //console.log(response.status + ": " + JSON.stringify(response.body));
-    //console.log("Response time was " + String(response.timings.duration) + " ms");
-    //console.log("Total timings: " + myTrend.add(response.timings.waiting));
-
     duration_trend.add(response.timings.duration)
     fail_rate.add(response.status !== 201)
 
